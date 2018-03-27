3dc79f5295edc01da5507ed2ffc0e431819e2ed5
Konstantin Stoianov <stoianovkonstantin00@gmail.com>
2018-03-27 20:31:12 +0300

    Updated 06.Good-Practice

---
+++ 06.Good-Practice/PhotoShare.Services/AuthenticationService.cs
@@ -0,0 +1,34 @@
+﻿using PhotoShare.Data;
+using PhotoShare.Models;
+using System.Linq;
+
+namespace PhotoShare.Services
+{
+    public class AuthenticationService
+    {
+        private static User loggedUser;
+
+        public static void Login(string username, string password)
+        {
+            using (PhotoShareContext context = new PhotoShareContext())
+            {
+                loggedUser = context.Users.FirstOrDefault(u => u.Username == username && u.Password == password);
+            }
+        }
+
+        public static void Logout()
+        {
+            loggedUser = null;
+        }
+
+        public static bool IsAuthenticated()
+        {
+            return loggedUser != null;
+        }
+
+        public static User GetCurrentUser()
+        {
+            return loggedUser;
+        }
+    }
+}
---
+++ 06.Good-Practice/PhotoShare.Services/UserService.cs
@@ -0,0 +1,34 @@
+﻿using PhotoShare.Data;
+using System.Linq;
+
+namespace PhotoShare.Services
+{
+    public class UserService
+    {
+        public bool UserExists(string username)
+        {
+            using (PhotoShareContext context = new PhotoShareContext())
+            {
+                return context.Users.Any(u => u.Username == username);
+            }
+        }
+
+        public bool CheckPassword(string username, string password)
+        {
+            using (PhotoShareContext context = new PhotoShareContext())
+            {
+                var user = context.Users.FirstOrDefault(u => u.Username == username);
+
+                if (user.Password == password)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+    }
+}
